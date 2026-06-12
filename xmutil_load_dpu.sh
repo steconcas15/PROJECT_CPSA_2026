339b3f110adfab126b51ed96bae42c847791eaaa
scaddeo24 <sara.caddeo24@gmail.com>
2026-06-12 10:20:54 +0200

    Add files via upload

---
+++ xmutil_load_dpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo xmutil unloadapp
+sleep 2
+sudo xmutil loadapp kv260-benchmark-b4096
+sleep 2
+sudo xmutil listapps
+sleep 2
+export XLNX_VART_FIRMWARE=/home/ubuntu/Desktop/PROJECT_CPSA_2026/DPU_FIRMWARE/kv260-benchmark-b4096/kv260-benchmark-b4096.xclbin
+sleep 2 
+echo $XLNX_VART_FIRMWARE
+sleep 2
