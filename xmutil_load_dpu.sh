65e38f95d89795672b66240315029cd81cb0c34f
Stefano Concas <stefanoconcas6@gmail.com>
2026-06-17 10:38:03 +0200

    Update xmutil_load_dpu.sh

--- xmutil_load_dpu.sh
+++ xmutil_load_dpu.sh
@@ -6,6 +6,6 @@ sleep 2
 sudo xmutil listapps
 sleep 2
 export XLNX_VART_FIRMWARE=/home/ubuntu/Desktop/PROJECT_CPSA_2026/DPU_FIRMWARE/kv260-benchmark-b4096/kv260-benchmark-b4096.xclbin
-sleep 2 
+sleep 2
 echo $XLNX_VART_FIRMWARE
 sleep 2
