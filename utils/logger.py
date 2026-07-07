8912ee6286dafda550586e22e7beaa6c9f505d44
fpiras00 <fpiras00@gmail.com>
2026-07-07 18:42:30 +0200

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,7 @@
 # logger.py
+#
+# Author: Stefano Concas, Matteo Matta, Sara Cadedo, Fabio Piras
+# Repository: https://github.com/steconcas15/PROJECT_CPSA_2026
 
 import os
 from datetime import datetime
