7550ad1f9b5f8cadc7b616e90818e690221963f4
fpiras00 <fpiras00@gmail.com>
2026-07-07 18:41:18 +0200

    Update audio_paths.py

--- utils/audio_paths.py
+++ utils/audio_paths.py
@@ -1,4 +1,7 @@
 # audio_paths.py
+#
+# Author: Stefano Concas, Matteo Matta, Sara Cadedo, Fabio Piras
+# Repository: https://github.com/steconcas15/PROJECT_CPSA_2026
 
 from pathlib import Path
 from utils.logger import log_system
